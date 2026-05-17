ce987f891169c724d399b03c6a8df5d0cbcef06b
Nate Martin <257912699+nathantyler12-bot@users.noreply.github.com>
2026-05-17 14:02:57 -0500

    Not really sure where I was so for posterity

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let chars = phrase.length;
+    let string = "";
+    for (let i=chars; i > 0; i--) {
+        string += phrase.substring(i,1)
+    }
+    return string
 };
 
 // Do not edit below this line

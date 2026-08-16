93fc288a19b01111c06abeca8274bcbf2463e8b1
Nate Martin <257912699+nathantyler12-bot@users.noreply.github.com>
2026-08-16 17:42:55 -0500

    Completed Exercised 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumberInput, secondNumberInput) {
+    if (firstNumberInput >= 0 && secondNumberInput >= 0 && Number.isInteger(firstNumberInput) == true 
+    && Number.isInteger(secondNumberInput) == true) {
+        let arrayOfNumbers = [firstNumberInput, secondNumberInput].toSorted((a,b) => a - b)
+        for (i=arrayOfNumbers[0] + 1; i < arrayOfNumbers[1]; i++) {
+            arrayOfNumbers.push(i)
+        }
+        initialValue = 0
+        let sum = arrayOfNumbers.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue)
+        return sum
+    } else return "ERROR"
 };
 
 // Do not edit below this line

ee49c2047587610f97d36c09bebe5ba869f572e9
Nate Martin <257912699+nathantyler12-bot@users.noreply.github.com>
2026-08-16 18:01:15 -0500

    Exercise 10 completed

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let yearDivisors = [4,100,400]
+    let remainders = yearDivisors.map((x) => year % x)
+    if (remainders[0] == 0 && remainders[1] != 0 && remainders[2] != 0) {
+        return true
+    } else if (remainders[0] == 0 && remainders[1] == 0 && remainders[2] == 0) {
+        return true
+    } else return false
 };
 
 // Do not edit below this line

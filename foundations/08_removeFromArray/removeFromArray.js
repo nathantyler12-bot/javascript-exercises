109b1100323ad9eae8e20a97748bba89b27297f4
Nate Martin <257912699+nathantyler12-bot@users.noreply.github.com>
2026-08-16 17:00:57 -0500

    Finished exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let newArray = array
+    for (arg of args) {
+        newArray = newArray.filter((element) => element !== arg)
+    } 
+    return newArray
 };
 
 // Do not edit below this line

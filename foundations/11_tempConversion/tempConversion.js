9d04629a9314a9e18c00195100c580c7b5bfab7f
Nate Martin <257912699+nathantyler12-bot@users.noreply.github.com>
2026-08-16 19:07:35 -0500

    Exercise 11 Complete

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitInput) {
+  celciusOutput = (fahrenheitInput - 32) * 5/9
+  if (celciusOutput != Math.round(celciusOutput)) {
+    celciusOutput = +celciusOutput.toFixed(1)
+  }
+  return celciusOutput
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusInput) {
+  fahrenheitOutput = (celciusInput * 9/5) + 32
+  if (fahrenheitOutput != Math.round(fahrenheitOutput)) {
+    fahrenheitOutput = +fahrenheitOutput.toFixed(1)
+  }
+  return fahrenheitOutput
 };
 
 // Do not edit below this line
